42677d2d3b1581d30e0012740c7c073b4f919bfd
Tien le <leminhtien151202@gmail.com>
2025-04-27 14:18:24 +0700

    update 1

--- library_backend/src/main/java/com/project/library_backend/dtos/BorrowStatusDTO.java
+++ library_backend/src/main/java/com/project/library_backend/dtos/BorrowStatusDTO.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data//toString
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
---
+++ library_backend/src/main/java/com/project/library_backend/dtos/BorrowerDTO.java
@@ -0,0 +1,24 @@
+package com.project.library_backend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data//toString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BorrowerDTO {
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDateTime createdAt;
+}
